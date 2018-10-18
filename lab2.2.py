9c2531be5c409a0363a824b38fd49686aa24b695
Bigoz005 <32684265+Bigoz005@users.noreply.github.com>
2018-10-18 14:39:32 +0200

    Update lab2.2.py

--- lab2.2.py
+++ lab2.2.py
@@ -5,8 +5,10 @@ my_array = array('i', [1, 1, 2, 2, 2, 4, 5, 6, 7, 8])
 for j in [0, 2, 4, 6, 8]:
 
     if my_array[j] == my_array[j + 1]:
+        print(my_array[j], my_array[j + 1])
         print("dobra para")
 
     else:
+        print(my_array[j], my_array[j + 1])
         print("zla para")
 
